662b6b4a34f6e17783a88d657763d1df68de29c7
kitsune2355 <pe3ka.63@gmail.com>
2025-06-18 11:25:28 +0700

    Set constants

---
+++ src/constant/ConstantItem.ts
@@ -0,0 +1,56 @@
+export const statusItems = {
+    pending:{
+        text: "รอดำเนินการ",
+        color: "#FFA500",
+        icon: "hourglass-outline",
+    },
+    success:{
+        text: "เสร็จสิ้น",
+        color: "#32CD32",
+        icon: "checkmark-circle-outline",
+    },
+    inprogress:{
+        text: "กำลังดำเนินการ",
+        color: "#1E90FF",
+        icon: "construct",
+    },
+}
+
+export const statusAll = [
+    {
+        text: "งานทั้งหมด",
+        color: "blue.500",
+        icon: "reader-outline",
+    },
+    {
+        text: "รอดำเนินการ",
+        color: "amber.500",
+        icon: "hourglass-outline",
+    },
+    {
+        text: "เสร็จสิ้น",
+        color: "green.500",
+        icon: "checkmark-circle-outline",
+    },
+]
+
+export const processItem = [
+  {
+    title: "แจ้งซ่อม",
+    icon: "build",
+  },
+  {
+    title: "ประวัติ",
+    icon: "time",
+  },
+  {
+    title: "ส่งงาน",
+    icon: "construct",
+  },
+];
+
+export const gradientcolorTheme = {
+      0: ["#006289", "#26a69a"],
+      1: ["#b13579", "#7e57c2"],
+      2: ["#3598b1", "#6f35b1"],
+    };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,117 +1,147 @@
 import React from "react";
-import { VStack, Text, Center, Icon, Flex, Box } from "native-base";
+import {
+  VStack,
+  Text,
+  Center,
+  Icon,
+  Flex,
+  Box,
+  HStack,
+  Badge,
+} from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { useTheme } from "../context/ThemeContext";
-
-const dashboard = [
-  {
-    title: "งานทั้งหมด",
-    value: 150,
-    icon: "reader-outline",
-    iconColor: "blue.500",
-  },
-  {
-    title: "รอดำเนินการ",
-    value: 10,
-    icon: "hourglass-outline",
-    iconColor: "amber.500",
-  },
-  {
-    title: "เสร็จสิ้น",
-    value: 10,
-    icon: "checkmark-circle-outline",
-    iconColor: "green.500",
-  },
-];
-
-const process = [
-  {
-    title: "แจ้งซ่อม",
-    icon: "build",
-  },
-  {
-    title: "ประวัติ",
-    icon: "time",
-  },
-  {
-    title: "ส่งงาน",
-    icon: "construct",
-  },
-];
+import { gradientcolorTheme, processItem, statusAll } from "../constant/ConstantItem";
 
 const HomeScreen: React.FC = () => {
   const { colorTheme } = useTheme();
 
-  const renderDashboardItem = (item: any) => (
-    <Box key={item.title} width="30%" mb="4">
-      <Center bg={colorTheme.colors.card} rounded="2xl" shadow={2} p="4" h="32">
-        <Icon
-          as={Ionicons}
-          name={item.icon}
-          size="8"
-          mb="2"
-          color={item.iconColor}
-        />
-        <Text
-          color="gray.500"
-          fontSize="sm"
-          fontWeight="medium"
-          mb="1"
-          textAlign="center"
-        >
-          {item.title}
-        </Text>
-        <Text
-          color="gray.800"
-          fontSize="lg"
-          fontWeight="bold"
-          textAlign="center"
-        >
-          {item.value}
-        </Text>
-      </Center>
-    </Box>
+  const renderDashboardItem = () => (
+    <>
+      {statusAll.map((st) => (
+        <Box key={st.text} width="30%" mb="4">
+          <Center
+            bg={colorTheme.colors.card}
+            rounded="2xl"
+            shadow={2}
+            p="4"
+            h="32"
+          >
+            <Icon
+              as={Ionicons}
+              name={st.icon}
+              size="8"
+              mb="2"
+              color={st.color}
+            />
+            <Text
+              color="gray.500"
+              fontSize="sm"
+              fontWeight="medium"
+              mb="1"
+              textAlign="center"
+            >
+              {st.text}
+            </Text>
+            <Text
+              color="gray.800"
+              fontSize="lg"
+              fontWeight="bold"
+              textAlign="center"
+            >
+              15
+            </Text>
+          </Center>
+        </Box>
+      ))}
+    </>
   );
 
   const renderProcessItem = (item: any, key: number) => {
-  const gradientcolorTheme = {
-    0: ["#006289", "#26a69a"],
-    1: ["#b13579", "#7e57c2"],
-    2: ["#3598b1", "#6f35b1"],
+    return (
+      <Box key={item.title} width="48%" mb="4">
+        <Box
+          bg={{
+            linearGradient: {
+              colors: gradientcolorTheme[key],
+              start: [0, 0],
+              end: [1, 1],
+            },
+          }}
+          rounded="3xl"
+          shadow={2}
+          p="4"
+          h="32"
+        >
+          <Center flex={1}>
+            <Icon as={Ionicons} name={item.icon} size="12" color="white" />
+            <Text
+              color="white"
+              fontSize="md"
+              mb="1"
+              textAlign="center"
+              fontWeight="bold"
+            >
+              {item.title}
+            </Text>
+          </Center>
+        </Box>
+      </Box>
+    );
   };
 
-  return (
-    <Box key={item.title} width="48%" mb="4">
-      <Box
-        bg={{
-          linearGradient: {
-            colors: gradientcolorTheme[key],
-            start: [0, 0],
-            end: [1, 1],
-          },
-        }}
-        rounded='3xl'
-        shadow={2}
-        p="4"
-        h="32"
-      >
-        <Center flex={1}>
-          <Icon as={Ionicons} name={item.icon} size="12" color="white" />
-          <Text
-            color="white"
-            fontSize="md"
-            mb="1"
-            textAlign="center"
-            fontWeight="bold"
-          >
-            {item.title}
-          </Text>
-        </Center>
-      </Box>
-    </Box>
-  );
-};
+  const renderActivityAll = () => {
+    return (
+      <Box bg={colorTheme.colors.card} rounded="2xl" shadow={2} p="4">
+        <HStack alignItems="flex-start" space={3}>
+          <Center bg="amber.50" rounded="full" size="10">
+            <Icon as={Ionicons} name="home" size="5" color="amber.500" />
+          </Center>
 
+          <VStack flex={1} space={2}>
+            <HStack>
+              <Text
+                color={colorTheme.colors.text}
+                fontSize="md"
+                fontWeight="bold"
+                numberOfLines={2}
+                ellipsizeMode="tail"
+                flex={1}
+              >
+                title
+              </Text>
+
+              <Badge
+                bgColor="amber.500"
+                variant="solid"
+                px={2}
+                py={0.5}
+                rounded="full"
+                _text={{ fontSize: "2xs", fontWeight: "bold", color: "white" }}
+              >
+                รอดำเนินการ
+              </Badge>
+            </HStack>
+
+            <Text
+              color={colorTheme.colors.text}
+              fontSize="sm"
+              numberOfLines={2}
+              ellipsizeMode="tail"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugit
+              sequi, exercitationem animi tempore harum ipsa dolore quasi
+              asperiores, eos tempora voluptatum iste ea vitae distinctio
+              delectus totam veritatis consequatur?
+            </Text>
+            <Text color="gray.500" fontSize="xs">
+              2025/06/18 12:00
+            </Text>
+          </VStack>
+        </HStack>
+      </Box>
+    );
+  };
 
   return (
     <VStack space={4}>
@@ -119,15 +149,20 @@ const HomeScreen: React.FC = () => {
         ภาพรวมงานซ่อม
       </Text>
       <Flex direction="row" wrap="wrap" justify="space-between">
-        {dashboard.map((item) => renderDashboardItem(item))}
+        {renderDashboardItem()}
       </Flex>
 
       <Text color={colorTheme.colors.text} fontSize="lg" fontWeight="bold">
         ดำเนินการด่วน
       </Text>
       <Flex direction="row" wrap="wrap" justify="space-between">
-        {process.map((item, key) => renderProcessItem(item, key))}
+        {processItem.map((item, key) => renderProcessItem(item, key))}
       </Flex>
+
+      <Text color={colorTheme.colors.text} fontSize="lg" fontWeight="bold">
+        กิจกรรมล่าสุด
+      </Text>
+      {renderActivityAll()}
     </VStack>
   );
 };
